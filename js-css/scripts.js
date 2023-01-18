742bd99d5f7aa3512d9dbe7499e61c2c0edaeaba
xescr <114061880+xescr@users.noreply.github.com>
2023-01-18 20:42:19 +0800

    committed testing version for help

--- js-css/scripts.js
+++ js-css/scripts.js
@@ -5,12 +5,12 @@ function gridsetup(){
         const gridelement = document.createElement('div');
         gridelement.classList.add('gridelement')
         gridcontainer.appendChild(gridelement)
-        gridelement.addEventListener(onmouseenter, function() {colorchange(gridelement)})
-        gridelement.addEventListener(onmouseleave, function() {colorchange2(gridelement)})
+        gridelement.addEventListener(onmouseenter, function() {test1()})
+        gridelement.addEventListener(onmouseleave, function() {test2()})
     }
     document.body.appendChild(gridcontainer)
 }
-
+// I won't show for space but test1 & 2 output 'meep1/2' to console.log
 function colorchange(title){
     title.target.style.backgroundColor(`hsl(${Math.random() * 360}, 100%, 50%`);
 }
@@ -18,6 +18,14 @@ function colorchange(title){
 function colorchange2(title){
     title.target.style.backgroundColor('#A9A9A9');
 }
+
+function test1(){
+    console.log("meep");
+}
+function test2(){
+    console.log("meep1");
+}
+
 
 
 gridsetup()
