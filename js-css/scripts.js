3409f2b8d6b2704b6b12c1a6d826af63fa5a3640
xescr <114061880+xescr@users.noreply.github.com>
2023-01-19 19:02:55 +0800

    finally fixed issue, had to do with quotations and made it one color only

--- js-css/scripts.js
+++ js-css/scripts.js
@@ -5,18 +5,18 @@ function gridsetup(){
         const gridelement = document.createElement('div');
         gridelement.classList.add('gridelement')
         gridcontainer.appendChild(gridelement)
-        gridelement.addEventListener(onmouseenter, function() {test1()})
-        gridelement.addEventListener(onmouseleave, function() {test2()})
+        gridelement.addEventListener('click', function() {colorchange(gridelement)})
+        gridelement.addEventListener('contextmenu', function() {colorchange2(gridelement)})
     }
     document.body.appendChild(gridcontainer)
 }
 // I won't show for space but test1 & 2 output 'meep1/2' to console.log
-function colorchange(title){
-    title.target.style.backgroundColor(`hsl(${Math.random() * 360}, 100%, 50%`);
+function colorchange(objitem){
+    objitem.classList.add("bluey")
 }
 
-function colorchange2(title){
-    title.target.style.backgroundColor('#A9A9A9');
+function colorchange2(objitem){
+    objitem.classList.remove("bluey");
 }
 
 function test1(){
